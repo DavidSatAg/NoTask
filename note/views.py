59a062fd8bcd4e02c14c808d8254038d765a1b20
inczDan <daniel.alvmatos@gmail.com>
2022-07-15 17:08:27 -0300

    att app note concluida

--- note/views.py
+++ note/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Anotacao
 
-from django.http import HttpResponse
-from .models import Document
 
 # Create your views here.
 
 # def note(request):
 #     return render(request, "note/index.html")
 
-def editor(request):
-    docid = int(request.GET.get('docid', 0))
-    documents = Document.objects.all()
-
-    context = {
-        'docid': docid,
-        'documents': documents
-    }
-
-    return render(request, 'note/editor.html', context)
+class AnotacaoList(ListView):
+    model = Anotacao
+    context_object_name = 'anotacoes'
+    template_name = 'note/editor.html'
