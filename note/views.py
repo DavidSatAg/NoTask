47c7c013b40ca6f3b9be4d038b013eca0f0b82b8
inczDan <daniel.alvmatos@gmail.com>
2022-07-16 23:44:52 -0300

    add class anotacaoDetail and AnotacaoCreate in app note

--- note/views.py
+++ note/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Anotacao
 
 
@@ -11,4 +12,9 @@ from .models import Anotacao
 class AnotacaoList(ListView):
     model = Anotacao
     context_object_name = 'anotacoes'
+    
+class AnotacaoDetail(DetailView):
+    model = Anotacao
+    context_object_name = 'anotacao'
     template_name = 'note/editor.html'
+
