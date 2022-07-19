c918d23ef2f10b9f55fe7b6d03cb5ded33c50743
David Aguina <aguinadavid@gmail.com>
2022-07-19 13:06:26 -0300

    teste

--- NoTask/urls.py
+++ NoTask/urls.py
@@ -1,5 +1,4 @@
 """NoTask URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
@@ -16,7 +15,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from note.views import AnotacaoList
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
--- home/views.py
+++ home/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 # Create your views here.
-
 def home(request):
     return render(request, 'home/index.html')
-
 def task(request):
     return render(request, '/home/davidaguina/Workspace/NoTask/note/templates/note/index.html')
