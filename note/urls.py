f7e4fa282abd4a8cc5917a12cbc13ac76730e979
Marcela Lima <marcelalima.df@gmail.com>
2022-07-19 14:38:12 -0300

    conectando o editor ao forms do django

--- note/urls.py
+++ note/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AnotacaoList
+from .views import AnotacaoList 
+from . import views
 
 urlpatterns = [
     # path('', views.note, name='note'),
-    path('', AnotacaoList.as_view(), name='anotacoes')
+    path('', AnotacaoList.as_view(), name='anotacoes'),
+    path('post/new/', views.post_new, name='post_new')
 ]
