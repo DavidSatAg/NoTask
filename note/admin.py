90698bc588cd50a40f8ae33a7ff4d3a352883ec7
Marcela Lima <marcelalima.df@gmail.com>
2022-07-25 15:39:41 -0300

    configura admin

--- note/admin.py
+++ note/admin.py
@@ -1,6 +1,7 @@
+
+
 from django.contrib import admin
 
-from .models import Anotacao
-# Register your models here.
+from .models import Note
 
-admin.site.register(Anotacao)
+admin.site.register(Note)
