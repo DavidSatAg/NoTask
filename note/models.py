a749adfc7ecc949d72ed9d3e98d600fd31c251fa
Marcela Lima <marcelalima.df@gmail.com>
2022-07-25 15:44:41 -0300

    template e estilo

--- note/models.py
+++ note/models.py
@@ -1,31 +1,8 @@
 from django.db import models
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
 
-class Anotacao(models.Model):
-    titulo = models.CharField(max_length=255)
-    conteudo = models.TextField(blank=True, null=True)
-    criado_em = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['criado_em']
-
-    def __str__(self):
-        return self.titulo
-
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
+class Meta:
+    ordering = ('title',)
