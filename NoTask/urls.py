abc664a81708ed82d7f477ef62b645a9232e615c
Marcela Lima <marcelalima.df@gmail.com>
2022-07-25 15:51:17 -0300

    alinhando os not found

--- NoTask/urls.py
+++ NoTask/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from note.views import AnotacaoList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
