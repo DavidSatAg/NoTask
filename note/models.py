1796e9a4032303c7d07be75b4c6b0014dc58d9ec
inczDan <daniel.alvmatos@gmail.com>
2022-07-26 14:49:53 -0300

    ajuste conexao url

--- note/models.py
+++ note/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
 
 class Meta:
     ordering = ('title',)
