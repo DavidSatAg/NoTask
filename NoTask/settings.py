a7af4f37eaaaed6ae2b260ec75a57502a86ca059
David Aguina <aguinadavid@gmail.com>
2022-07-28 14:25:04 -0300

    texto

--- NoTask/settings.py
+++ NoTask/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 #auth_user validation
 
